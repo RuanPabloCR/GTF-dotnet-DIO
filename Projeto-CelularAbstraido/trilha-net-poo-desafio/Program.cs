f52b43db69e9350be46cae727b35b993aa69fbdc
Ruan Pablo <ruanpablocutrimramon@gmail.com>
2025-07-30 14:17:21 +0000

    desafio2

---
+++ Projeto-CelularAbstraido/trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using DesafioPOO.Models;
+
+namespace Projeto_Celular
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia:");
+            Smartphone nokia = new Nokia("11999999999", "Nokia 3310", "1234567890", 123);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine("\nSmartphone Iphone:");
+            Smartphone iphone = new Iphone("11988888888", "Iphone 12", "0987654321", 256);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("WhatsApp2");
+
+        }
+    }
+}
